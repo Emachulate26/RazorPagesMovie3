6920b1a8e8a944d71422533e3aa55b45fc78d73a
10Ngwako <ngoakoramakgoakgoa@gmail.com>
2025-11-26 15:04:39 +0200

    added bookings.cs

---
+++ RazorPagesMovie/Models/Booking.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+
+        public int MovieTimeSlotId { get; set; }
+        public MovieTimeSlot MovieTimeSlot { get; set; }
+
+        [Required]
+        public string UserName { get; set; } = string.Empty;
+    }
+}
