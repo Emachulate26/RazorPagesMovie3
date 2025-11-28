9ea4b98ceb331e9e33b021d897e0ba93d28945a1
pheagane rabodiba <samrabodiba22@gmail.com>
2025-11-28 09:39:38 +0200

    This stores is foe  each user for bookings

---
+++ RazorPagesMovie/Models/Booking.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int ShowtimeId { get; set; }
+        public Showtime Showtime { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public int SeatsBooked { get; set; }
+    }
+}
