cee811bbcd8471f7743f8986c7d2fc37b764217d
pheagane rabodiba <samrabodiba22@gmail.com>
2025-11-28 09:35:02 +0200

    Adding default movies in the seeddata for people to book on time

---
+++ RazorPagesMovie/Models/Showtime.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.Models
+{
+    public class Showtime
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int MovieId { get; set; }
+        public Movie Movie { get; set; }
+
+        [Required]
+        [Display(Name = "Show Time")]
+        public DateTime Time { get; set; }
+
+        [Required]
+        public string Location { get; set; }  // Cinema / Room
+
+        [Display(Name = "Total Seats")]
+        public int TotalSeats { get; set; } = 50;
+
+        [Display(Name = "Booked Seats")]
+        public int BookedSeats { get; set; } = 0;
+    }
+}
