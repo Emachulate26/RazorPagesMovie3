8a2ced56a390e0e0418b4441f1b24fb463cd7f5a
Emachulate26 <kgadiemma77@gmail.com>
2025-12-03 08:48:31 +0200

    adding a REGISTER model .cs

---
+++ RazorPagesMovie/Models/RegisterModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.Models
+{
+    public class RegisterModel
+    {
+        [Required]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
--- RazorPagesMovie/Pages/Register.cshtml.cs
+++ RazorPagesMovie/Pages/Register.cshtml.cs
@@ -1,12 +1,35 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesMovie.Models;
+using RazorPagesMovie.Services;
 
 namespace RazorPagesMovie.Pages
 {
-    public class RegisterModel : PageModel
+    public class RegisterModelPage : PageModel
     {
+        private readonly UserService _userService;
+
+        public RegisterModelPage(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        [BindProperty]
+        public RegisterModel Input { get; set; } = new RegisterModel();
+
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+                return Page();
+
+            _userService.AddUser(Input.Username, Input.Password);
+
+            return RedirectToPage("/Index");
         }
     }
 }
